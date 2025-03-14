b2287ead07341368a9328c4e6db998be20c37dc5
meeeiiike <g00407095@atu.ie>
2025-03-14 23:44:10 +0000

    created update class

---
+++ src/main/java/ie/atu/GroupProject/StudentManagementUpdate.java
@@ -0,0 +1,13 @@
+package ie.atu.GroupProject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class StudentManagementUpdate {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+
+}

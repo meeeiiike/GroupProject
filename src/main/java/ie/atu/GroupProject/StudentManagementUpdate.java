afa9b7d96787abf9572fc49fd88e6300bfaea055
meeeiiike <g00407095@atu.ie>
2025-03-17 17:30:41 +0000

    Created StudentManagementUpdate which connects to our DB using the properties file (store sensitive info), i then have a simple, single query which updates the name of a department (ATU -> NUIG), i then connect to the db and execute the update query and print a message showing how many rows are deleted.
    TODO: will need to add functionality to update any table, along with specific fields. this will include user input and prepared statements.

--- src/main/java/ie/atu/GroupProject/StudentManagementUpdate.java
+++ src/main/java/ie/atu/GroupProject/StudentManagementUpdate.java
@@ -1,10 +1,44 @@
 package ie.atu.GroupProject;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class StudentManagementUpdate {
     public static void main(String[] args){
-        System.out.println("Hello World!!!");
-    }
 
+        // Get database properties
+        Properties dbProps = new Properties();
+        try(InputStream input = TestConnection.class.getResourceAsStream("/db.properties")){
+            if(input== null){
+                System.out.println("Unable to find db.properties");
+                return;
+            }
+            dbProps.load(input);
+        } catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
+        String url = dbProps.getProperty("db.url");
+        String username = dbProps.getProperty("db.username");
+        String password = dbProps.getProperty("db.password");
+
+        // Update Query
+        String updateSQL = "UPDATE department SET name = 'NUIG' WHERE name = 'ATU'";
+
+//String updateSQL = "UPDATE department SET name = '?' WHERE name = '?'";
+//Layout to use when we implement code to ask user for input
+//Will need to use PreparedStatements!
+
+        // Create Connection using db.properties
+        try(Connection con = DriverManager.getConnection(url, username, password)){
+            Statement stmt = con.createStatement();
+            int rowsUpdated = stmt.executeUpdate(updateSQL);
+            System.out.println("Rows Updated Successfully: " +rowsUpdated);
+        } catch(SQLException e){
+            e.printStackTrace();
+            System.out.println("Could not Connect");
+        }
+    }
 }

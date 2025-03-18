7003f4e143d5ebed268f615f104d63c186307240
meeeiiike <g00407095@atu.ie>
2025-03-18 21:31:10 +0000

    added comments and finished code that never got committed earlier ( switch cases )

--- src/main/java/ie/atu/GroupProject/StudentManagementUpdate.java
+++ src/main/java/ie/atu/GroupProject/StudentManagementUpdate.java
@@ -47,6 +47,8 @@ public class StudentManagementUpdate {
                     //TODO: add error msg when we add ID that doesnt exist yet
                     // add same functionality for all switch cases
                     // test for failure, add exceptions and error logs
+                    // Add connection pool!
+                    // Use Code Coverage to help with Testing! Complexity < 10
                     // LOTS OF REDUNDANT CODE! once happy with functionality,
                     // Clean up code to reduce redundancy
 
@@ -86,11 +88,67 @@ public class StudentManagementUpdate {
                     }
                     break;
                 case 3:
-                    System.out.println("Update Staff\nPlease enter ID: ");
+                    System.out.println("Update staff\nPlease enter ID: ");
+                    int staffID = sc.nextInt();
+                    try (Connection con = DriverManager.getConnection(url, username, password)) {
+                        PreparedStatement stmt = con.prepareStatement("UPDATE staff SET first_name = ?, last_name = ?, email = ?, phone_number = ?, office_location = ? WHERE staff_id = " + staffID);
+                        System.out.println("Enter first name: ");
+                        String first_name = sc.next();
+                        System.out.println("Enter last name: ");
+                        String last_name = sc.next();
+                        System.out.println("Enter email: ");
+                        String email = sc.next();
+                        System.out.println("Enter phone: ");
+                        String phone  = sc.next();
+                        System.out.println("Enter office: ");
+                        String office = sc.next();
+                        stmt.setString(1, first_name);
+                        stmt.setString(2, last_name);
+                        stmt.setString(3, email);
+                        stmt.setString(4, phone);
+                        stmt.setString(5, office);
+
+                        int rowsUpdated = stmt.executeUpdate();
+                        System.out.println("Rows Updated Successfully: " + rowsUpdated);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        System.out.println("Could not Connect");
+                    }
                     break;
                 case 4:
-                    System.out.println("Update Course");
+                    System.out.println("Update Course\nPlease enter ID: ");
+                    int courseID = sc.nextInt();
+                    try (Connection con = DriverManager.getConnection(url, username, password)) {
+                        PreparedStatement stmt = con.prepareStatement("UPDATE course SET course_name = ?, credits = ?, level = ?, semester = ?, duration_weeks = ?,  max_students = ? WHERE course_id = " + courseID);
+                        System.out.println("Enter course name: ");
+                        String course_name = sc.next();
+                        System.out.println("Enter credits: ");
+                        int credits = sc.nextInt();
+                        System.out.println("Enter level: ");
+                        int level = sc.nextInt();
+                        System.out.println("Enter semester: ");
+                        int semester = sc.nextInt();
+                        System.out.println("Enter duration in weeks: ");
+                        int duration_weeks = sc.nextInt();
+                        System.out.println("Enter Max Students: ");
+                        int max_students = sc.nextInt();
+
+                        stmt.setString(1, course_name);
+                        stmt.setInt(2, credits);
+                        stmt.setInt(3, level);
+                        stmt.setInt(4, semester);
+                        stmt.setInt(5, duration_weeks);
+                        stmt.setInt(6, max_students);
+
+                        int rowsUpdated = stmt.executeUpdate();
+                        System.out.println("Rows Updated Successfully: " + rowsUpdated);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        System.out.println("Could not Connect");
+                    }
                     break;
+
+                //TODO write test units FIRST for rest of switch cases, then add test units for existing code
                 case 5:
                     System.out.println("Update Grades");
                     break;

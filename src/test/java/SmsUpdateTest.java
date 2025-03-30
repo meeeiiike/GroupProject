e7df8746953f6e54650778bc4af1196c410b8d0e
meeeiiike <g00407095@atu.ie>
2025-03-30 20:01:45 +0100

    testing merge for fergal

--- src/test/java/SmsUpdateTest.java
+++ src/test/java/SmsUpdateTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+// testing merge for fergal
+
 public class SmsUpdateTest {
     @Test
     void testUpdateDepartmentSuccess() throws SQLException {

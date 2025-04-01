9a2e75e6613e3f85bea1ff3b8660d82053178e80
seankeogh1 <G00432475@atu.ie>
2025-04-01 14:11:13 +0100

    Finished Junit

--- src/test/java/SmsReadTest.java
+++ src/test/java/SmsReadTest.java
@@ -10,17 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SmsReadTest {
 
-   @Test
-    void testDepartmentReadSuccess()throws SQLException{
-       String url = "jdbc:mysql://localhost:3306/sms";
-       String username = "root";
-       String password = "password";
+    @Test
+    void testDepartmentReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
 
-       StudentManagementRead.ReadDepartment(url, username, password);
-   }
+        StudentManagementRead.ReadDepartment(url, username, password);
+    }
 
     @Test
-    void testDepartmentReadFailure()throws SQLException{
+    void testDepartmentReadFailure() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/ssms";
         String username = "root";
         String password = "password";
@@ -29,5 +29,142 @@ public class SmsReadTest {
         assertEquals("Error: SQL connection not found.", ex.getMessage());
 
     }
+
+    @Test
+    void testStudentReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
+
+        StudentManagementRead.ReadStudent(url, password, username);
+    }
+
+    @Test
+    void testStudentReadFailure() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ssms";
+        String username = "root";
+        String password = "password";
+
+        Exception ex = assertThrows(SQLSyntaxErrorException.class, () -> StudentManagementRead.ReadStudent(url, password, username));
+        assertEquals("Error: SQL connection not found.", ex.getMessage());
+    }
+
+    @Test
+    void testStaffReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
+
+        StudentManagementRead.ReadStaff(url, username, password);
+    }
+
+    @Test
+    void testStaffReadFailure() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ssms";
+        String username = "root";
+        String password = "password";
+
+        Exception ex = assertThrows(SQLSyntaxErrorException.class, () -> StudentManagementRead.ReadStaff(url, password, username));
+        assertEquals("Error: SQL connection not found.", ex.getMessage());
+    }
+
+    @Test
+    void testCourseReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
+
+        StudentManagementRead.ReadCourse(url, username, password);
+    }
+
+    @Test
+    void testCourseReadFailure() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ssms";
+        String username = "root";
+        String password = "password";
+
+        Exception ex = assertThrows(SQLSyntaxErrorException.class, () -> StudentManagementRead.ReadCourse(url, password, username));
+        assertEquals("Error: SQL connection not found.", ex.getMessage());
+    }
+
+    @Test
+    void testGradesReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
+
+        StudentManagementRead.ReadGrades(url, username, password);
+    }
+
+    @Test
+    void testGradesReadFailure() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ssms";
+        String username = "root";
+        String password = "password";
+
+        Exception ex = assertThrows(SQLSyntaxErrorException.class, () -> StudentManagementRead.ReadGrades(url, password, username));
+        assertEquals("Error: SQL connection not found.", ex.getMessage());
+    }
+    @Test
+    void testPaymentsReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
+
+        StudentManagementRead.ReadPayments(url, username, password);
+    }
+
+    @Test
+    void testPaymentsReadFailure() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ssms";
+        String username = "root";
+        String password = "password";
+
+        Exception ex =assertThrows(SQLSyntaxErrorException.class, () -> StudentManagementRead.ReadPayments(url, password, username));
+        assertEquals("Error: SQL connection not found.", ex.getMessage());
+    }
+
+    @Test
+    void testStudAddressReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
+
+        StudentManagementRead.ReadStudAddress(url,username,password);
+    }
+
+    @Test
+    void testStudAddressReadFailure() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ssms";
+        String username = "root";
+        String password = "password";
+
+        Exception ex = assertThrows(SQLSyntaxErrorException.class, () -> StudentManagementRead.ReadStudAddress(url,password,username));
+        assertEquals("Error: SQL connection not found.", ex.getMessage());
+    }
+
+    @Test
+    void testColAddressReadSuccess() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sms";
+        String username = "root";
+        String password = "password";
+
+        StudentManagementRead.ReadColAddress(url,username,password);
+    }
+
+    @Test
+    void testColAddressReadFailure() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ssms";
+        String username = "root";
+        String password = "password";
+
+        Exception ex = assertThrows(SQLSyntaxErrorException.class, () -> StudentManagementRead.ReadColAddress(url,password,username));
+        assertEquals("Error: SQL connection not found.", ex.getMessage());
+    }
+
+
+
+
+
 
 }
